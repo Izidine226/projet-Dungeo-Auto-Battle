8be4e14bbdf67f448ac756721b3baf9ac59cb485
Izidine Nebie <nebieizidinemagid@gmail.com>
2026-01-18 13:45:27 +0100

    Ajout classes Heros et Factory complete

---
+++ Guerrier.h
@@ -0,0 +1,37 @@
+#ifndef GUERRIER_H
+#define GUERRIER_H
+
+#include "Hero.h" 
+#include <iostream>
+
+using namespace std;
+
+class Guerrier : public Hero {
+public:
+    // Constructeur : Il reçoit les stats et les passe directement au constructeur de Hero
+    Guerrier(string nom, int pv, int attaque, int defense, int vitesse) 
+        : Hero(nom, pv, attaque, defense, vitesse) {}
+
+   
+    // 1 = Guerrier
+    int getClasse() const override { 
+        return 1; 
+    }
+
+    //  gestion des dégâts
+    int calculerDegats(const Hero& cible) override {
+        // Formule de base (Attaque / Defense de la cible)
+        // On utilise max(1, ...) pour ne jamais diviser par 0
+        int degats = attaque / max(1, cible.getDefense());
+
+        // Bonus Spécial Guerrier : +20% dégâts si PV < 30% 
+        if (pv < (pvMax * 0.3)) {
+            degats = (int)(degats * 1.2); 
+            cout << "RAGE , " << nom << " est en furie ! Ses degats augmentent !" << endl;
+        }
+
+        return degats;
+    }
+};
+
+#endif
